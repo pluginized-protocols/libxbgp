a2289acf4135a452cf7329307474b041cd220670
twirtgen <thomas.wirtgen@uclouvain.be>
2021-08-06 14:16:05 +0200

    Add RR native scenarios in the get_scenario function

--- misc/experiments/scenario/__init__.py
+++ misc/experiments/scenario/__init__.py
@@ -3,10 +3,15 @@ from typing import Sequence, Callable
 from misc.experiments.scenario.native_scenario import scenario_frr_native, scenario_bird_native
 from misc.experiments.scenario.rr_scenario import scenario_frr_plugin_route_reflector_memcheck, \
     scenario_bird_plugin_route_reflector_memcheck, scenario_frr_plugin_route_reflector_no_memcheck, \
-    scenario_bird_plugin_route_reflector_no_memcheck
+    scenario_bird_plugin_route_reflector_no_memcheck, scenario_bird_native_route_reflector, \
+    scenario_frr_native_route_reflector
 
 
 def get_scenarios() -> Sequence[Callable]:
-    return [scenario_frr_native, scenario_bird_native, scenario_frr_plugin_route_reflector_memcheck,
-            scenario_frr_plugin_route_reflector_no_memcheck, scenario_bird_plugin_route_reflector_memcheck,
+    return [scenario_frr_native, scenario_bird_native,
+            scenario_frr_native_route_reflector,
+            scenario_bird_native_route_reflector,
+            scenario_frr_plugin_route_reflector_memcheck,
+            scenario_frr_plugin_route_reflector_no_memcheck,
+            scenario_bird_plugin_route_reflector_memcheck,
             scenario_bird_plugin_route_reflector_no_memcheck]
