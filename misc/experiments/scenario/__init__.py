3e63cccbbcaf5029f74700a772b22d3fc2b2b5a9
twirtgen <thomas.wirtgen@uclouvain.be>
2021-09-09 10:32:08 +0200

    Add rpki scenario

--- misc/experiments/scenario/__init__.py
+++ misc/experiments/scenario/__init__.py
@@ -1,6 +1,9 @@
 from typing import Sequence, Callable
 
 from misc.experiments.scenario.native_scenario import scenario_frr_native, scenario_bird_native
+from misc.experiments.scenario.rpki_scenario import scenario_bird_prefix_validation_memcheck, \
+    scenario_bird_prefix_validation_no_memcheck, scenario_frr_prefix_validation_memcheck, \
+    scenario_frr_prefix_validation_no_memcheck
 from misc.experiments.scenario.rr_scenario import scenario_frr_plugin_route_reflector_memcheck, \
     scenario_bird_plugin_route_reflector_memcheck, scenario_frr_plugin_route_reflector_no_memcheck, \
     scenario_bird_plugin_route_reflector_no_memcheck, scenario_bird_native_route_reflector, \
@@ -16,4 +19,8 @@ def get_scenarios() -> Sequence[Callable]:
             scenario_frr_plugin_route_reflector_memcheck,
             scenario_frr_plugin_route_reflector_no_memcheck,
             scenario_bird_plugin_route_reflector_memcheck,
-            scenario_bird_plugin_route_reflector_no_memcheck]
+            scenario_bird_plugin_route_reflector_no_memcheck,
+            scenario_bird_prefix_validation_memcheck,
+            scenario_bird_prefix_validation_no_memcheck,
+            scenario_frr_prefix_validation_memcheck,
+            scenario_frr_prefix_validation_no_memcheck]
