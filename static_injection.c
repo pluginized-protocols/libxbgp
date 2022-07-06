bedbfa4ac6dc183b2b106332847ca862cd5f06e8
twirtgen <thomas.wirtgen@uclouvain.be>
2022-07-06 15:24:01 +0200

    the memory management is global to the plugin, and not per pluglet!

--- static_injection.c
+++ static_injection.c
@@ -409,9 +409,7 @@ static int obj_code_list_parser(json_object *obj_code_list, struct obj_code_list
     int path_code_len;
     j_obj(permissions);
     j_obj(add_memcheck);
-    j_obj(const char *, memory_mgmt);
     j_obj(int, use_libffi);
-    mem_type_t mt;
 
     obj_iter_end = json_object_iter_end(obj_code_list);
 
@@ -437,18 +435,6 @@ static int obj_code_list_parser(json_object *obj_code_list, struct obj_code_list
             curr_code->permissions = parse_permissions_pluglet(j_obj_json(permissions));
         } else {
             curr_code->permissions = 0;
-        }
-
-        if (j_obj_get_ex(j_obj_json(obj_code), memory_mgmt)) {
-            j_obj_val(memory_mgmt) = json_object_get_string(j_obj_json(memory_mgmt));
-            mt = str_memtype_to_enum(j_obj_val(memory_mgmt));
-            if (mt == MIN_MEM) {
-                fprintf(stderr, "Unknown memory manager: \"%s\" !\n", j_obj_val(memory_mgmt));
-                return -1;
-            }
-            curr_code->memory_mgt = mt;
-        } else {
-            curr_code->memory_mgt = BUMP_MEM;
         }
 
         if (j_obj_get_ex(j_obj_json(obj_code), use_libffi)) {
@@ -506,6 +492,9 @@ static int global_opts_parser(json_object *main_json, struct global_opts_parser 
     /* plugin name */
     j_obj(const char *, name);
     size_t name_len;
+    /* extra memory mgmt */
+    j_obj(const char *, memory_mgmt);
+    mem_type_t mt;
 
     /* 1. Parse JIT value */
     if (j_obj_get_ex(main_json, jit_all)) {
@@ -573,6 +562,19 @@ static int global_opts_parser(json_object *main_json, struct global_opts_parser 
     } else {
         // 4096 extra memory bytes will be allocated to the pluglets of the plugin
         g_parser->extra_mem = 4096;
+    }
+
+    /* 6. memory management for extra mem */
+    if (j_obj_get_ex(main_json, memory_mgmt)) {
+        j_obj_val(memory_mgmt) = json_object_get_string(j_obj_json(memory_mgmt));
+        mt = str_memtype_to_enum(j_obj_val(memory_mgmt));
+        if (mt == MIN_MEM) {
+            fprintf(stderr, "Unknown memory manager: \"%s\" !\n", j_obj_val(memory_mgmt));
+            return -1;
+        }
+        g_parser->memory_mgt = mt;
+    } else {
+        g_parser->memory_mgt = BUMP_MEM;
     }
 
     return 0;
@@ -672,7 +674,7 @@ static int load_pluglet(const char *path, const char *extension_code_dir,
                                curr_iter->name_insertion_len, curr_iter->anchor, curr_iter->seq,
                                obj_code_info->jit, obj_code_info->path_code, 0, curr_iter->pluglet_name,
                                curr_iter->pluglet_name_len, api_proto, obj_code_info->permissions,
-                               obj_code_info->add_memchecks, obj_code_info->memory_mgt, obj_code_info->use_libffi) != 0) {
+                               obj_code_info->add_memchecks, g_parser.memory_mgt, obj_code_info->use_libffi) != 0) {
             goto end;
         }
 
